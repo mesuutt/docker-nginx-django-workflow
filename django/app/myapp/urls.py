ad5befc71d184b92e51efeed7162a573a3cb2acc
Mesut Tasci <slackermesut@gmail.com>
2016-07-07 22:51:55 +0300

    HostInfoView Added

--- django/app/myapp/urls.py
+++ django/app/myapp/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from core.views import HostInfoView
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', HostInfoView.as_view()),
 ]
