08bc97d933203b9890fc9ecb53538472df1c5edf
Mesut Tasci <slackermesut@gmail.com>
2016-07-08 01:42:14 +0300

    Add project dir to path

--- django/app/myapp/wsgi.py
+++ django/app/myapp/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+from os.path import abspath, dirname
+from sys import path
 
 from django.core.wsgi import get_wsgi_application
+
+
+SITE_ROOT = dirname(abspath(__file__))
+path.append(SITE_ROOT)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myapp.settings")
 
